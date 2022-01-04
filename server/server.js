939484fb9a5a3287a610306403d04c431743eb67
davegrnr <davegrnr@gmail.com>
2022-01-04 15:10:51 -0500

    Migrate from create-react-app-buildpack to Node server

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const app = require("./app");
 const PORT = +process.env.PORT || 3001;
 
 app.listen(PORT, function () {
-    console.log(`Started on http://localhost:${PORT}`);
+    console.log(`Started on ${PORT}`);
 });
 
 app.get("/api", (req, res) => {
