d793aaa72cffcc5194f796af7288d41bffb0f69a
davegrnr <davegrnr@gmail.com>
2022-01-05 10:49:37 -0500

    Edit server.js for pathing

--- server/app.js
+++ server/app.js
@@ -27,7 +27,6 @@ app.use("/services", servicesRoutes);
 app.use("/sales", salesRoutes);
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, 'build')));
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'build', 'index.html'))
     })
