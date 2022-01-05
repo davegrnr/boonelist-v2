c4ba47c5ae19e3847d2a79f8e0d9c1e5e312c82d
davegrnr <davegrnr@gmail.com>
2022-01-05 11:21:28 -0500

    Edit API endpoints to remove extra /

--- server/app.js
+++ server/app.js
@@ -22,13 +22,12 @@ app.use(express.json());
 app.use(morgan("tiny"));
 app.use(authenticateJWT);
 
-app.use("/auth", authRoutes);
+app.use("auth", authRoutes);
 app.use("/users", usersRoutes);
 app.use("/services", servicesRoutes);
 app.use("/sales", salesRoutes);
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, 'build')));
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'build', 'index.html'))
     })
