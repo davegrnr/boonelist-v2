6a7a020a46b57f078974c12fef30b23fbfcff1ae
davegrnr <davegrnr@gmail.com>
2022-01-07 14:08:49 -0500

    Change some server scripts

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const app = require("./app");
 // const { PORT } = require("./config");
 const PORT = +process.env.PORT || 3001;
 
+app.use(express.static(path.join(__dirname, 'client/build')))
+
 app.listen(PORT, function () {
     console.log(`Started on ${PORT}`);
 });
